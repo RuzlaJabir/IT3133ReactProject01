42f726df26e45ae720e515d797a9498d4e2b59a9
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2024-11-23 19:09:23 +0000

    Profile component added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,12 @@
+export default function Profile(props){
+    return(
+        <>
+            <table>
+                <tr>
+                    <td rowSpan={7}> <img src={require('../assets/img/1.jpg')} alt="profile picture" /></td>
+                    <td>ID {}</td>
+                </tr>
+            </table>
+        </>
+    );
+}
